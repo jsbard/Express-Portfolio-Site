bc0fd173aef99ac4dad0170961f966902fa269f0
Andrew Hickman <hello@andrewhickman.me>
2021-06-27 22:12:52 -0400

    Add configs for Heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const data = require("./data.json");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "pug");
 app.use('/static', express.static('public'));
 
