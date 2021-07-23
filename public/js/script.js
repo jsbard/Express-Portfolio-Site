5bd3ca86a9f3075fc6c4cb649a8d39b1a1caa460
Andrew Hickman <hello@andrewhickman.me>
2021-07-23 16:01:14 -0400

    Fix error with loading icon not disappearing on project iframe load

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,14 @@
 const body = document.querySelector('body');
 let headerBtnClicked = false;
 
+
+/** Stop see-through loading icon in project iframe **/
+const iframe = document.querySelector('.project-embed');
+
+(() => iframe.onload = () => {
+  iframe.style.background = "none";
+})();
+
 document.querySelector('#menu-icon').addEventListener('click', e => {
   !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
   return headerBtnClicked = !headerBtnClicked;
@@ -16,9 +24,3 @@ document.querySelector('.portfolio-intro').addEventListener('click', e => {
   !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
   return headerBtnClicked = !headerBtnClicked;
 });
-
-/** Stop see-through loading icon in project iframe **/
-const iframe = document.getElementsByTagName('iframe')[0];
-(() => iframe.onload = () => {
-  iframe.style.background = "none";
-})();
