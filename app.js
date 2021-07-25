3d0deb0271722af370902b4e4579dc5b7c686e2b
Andrew Hickman <hello@andrewhickman.me>
2021-07-25 15:21:21 -0400

    Add contact section sidebar and forms

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const data = require("./data.json");
 const app = express();
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
+const emailAuth = require("./config");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // Specify port for deployment to Heroku
 let port = process.env.PORT || 3000;
@@ -16,12 +22,44 @@ app.get("/about", (req, res) => {
    res.render("about");
 });
 
+app.post('/contact-email', function (req, res) {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            // should be replaced with real sender's account
+            user: "landandleap@gmail.com",
+            pass: "Mermavope21$"
+        }
+    });
+
+    let mailOptions = {
+            from: 'landandleap@gmail.com',  // sender address
+            to: 'landandleap@gmail.com',   // list of receivers
+            subject: 'Sending Email using Node.js',
+            text: 'That was easy!',
+            html: '<b>Hey there from Nodemailer! </b>'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+
+    res.send();
+    res.end();
+});
+
 // For every project in data.json, create a route for the project and use the project.pug template for each
 for (let i=0; i<data.projects.length; i++){
    app.get(`/project/${data.projects[i].id}`, (req, res) => {
       res.render("project", data.projects[i]);
    });
 }
+
 
 // Handle 404 errors specifically
 app.use((req, res, next) => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,119 @@
-
 'use strict';
 
 /**
  * Handle mobile menu functionality to hide/reveal sidebar on mobile layouts
  */
 const body = document.querySelector('body');
+const iframe = document.querySelector('.project-embed');
+const menuIcon = document.getElementById("menu-icon");
+const contactSidebarCloseBtn = document.querySelector(".close-contact-sidebar");
+const contactSidebar = document.getElementById('contact-sidebar');
+const socialButtons = document.querySelector(".contact.social-icons");
+const socialSections = document.querySelectorAll(".social-info div");
 let headerBtnClicked = false;
+let contactToggleClicked = false;
 
 
 /** Stop see-through loading icon in project iframe **/
-const iframe = document.querySelector('.project-embed');
 
-(() => iframe.onload = () => {
-  iframe.style.background = "none";
-})();
+if (iframe) {
+  (() => iframe.onload = () => {
+    iframe.style.background = "none";
+  })();
+}
+
+/** Handle toggle event for right sidebar **/
+
+if (contactSidebar) {
+  document.querySelector('.contact-toggle').addEventListener('click', e => {
+    if (!contactToggleClicked){
+      contactSidebar.style.width = "300px";
+      contactSidebar.style.padding = "20px";
+      contactSidebarCloseBtn.style.display = "block";
+    } else {
+      contactSidebar.style.width = "0px";
+      contactSidebar.style.padding = "0px";
+      contactSidebarCloseBtn.style.display = "none";
+    }
+    return contactToggleClicked = !contactToggleClicked;
+  });
+
+  // Handle toggle events for social contact buttons
+    const githubButton = document.querySelector(".contact.social-icons .icon-github");
+    const linkedinButton = document.querySelector(".contact.social-icons .icon-linkedin");
+    const twitterButton = document.querySelector(".contact.social-icons .icon-twitter");
+    const github = document.querySelector(".social-info .github");
+    const linkedin = document.querySelector(".social-info .linkedin");
+    const twitter = document.querySelector(".social-info .twitter");
+
+    socialButtons.addEventListener("click", (e) => {
+      socialSections.forEach(section => {
+        section.style.display = "none";
+        section.style.maxHeight = "0";
+      });
+
+      if (e.target === githubButton){
+        github.style.display = "block";
+        github.style.maxHeight = "auto";
+      }
+
+      if (e.target === linkedinButton){
+        linkedin.style.display = "block";
+        linkedin.style.maxHeight = "auto";
+      }
+
+      if (e.target === twitterButton){
+        twitter.style.display = "block";
+        twitter.style.maxHeight = "auto";
+      }
+  });
+
+    //Contact form client configurations
+  const contactFormEmail = document.querySelector("#contact-form-email");
+  contactFormEmail.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/contact-email", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4){
+        console.log("Nice!");
+      }
+    }
+    xhr.send();
+    // xhr.send(JSON.stringify({
+    //   value: value
+    // }));
+  });
+}
+
+if (document.querySelector(".portfolio-intro")) {
+  document.querySelector('.portfolio-intro').addEventListener('click', e => {
+    if (headerBtnClicked) {
+      menuIcon.classList.remove("icon-chevron-thin-left");
+      menuIcon.style.borderTop = "2px solid #fefefe";
+      menuIcon.style.borderBottom = "2px solid #fefefe";
+      menuIcon.style.position = "relative";
+      body.style.transform = 'translateX(0px)';
+    }
+    return headerBtnClicked = !headerBtnClicked;
+  });
+}
 
 document.querySelector('#menu-icon').addEventListener('click', e => {
-  !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
-  return headerBtnClicked = !headerBtnClicked;
-});
 
-document.querySelector('.portfolio-intro').addEventListener('click', e => {
-  !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
+  if (headerBtnClicked) {
+    menuIcon.style.borderTop = "0px";
+    menuIcon.style.borderBottom = "0px";
+    menuIcon.classList.add("icon-chevron-thin-left");
+    menuIcon.style.position = "static";
+    body.style.transform = 'translateX(300px)';
+  } else {
+    menuIcon.classList.remove("icon-chevron-thin-left");
+    menuIcon.style.borderTop = "2px solid #fefefe";
+    menuIcon.style.borderBottom = "2px solid #fefefe";
+    menuIcon.style.position = "relative";
+    body.style.transform = 'translateX(0px)';
+  }
   return headerBtnClicked = !headerBtnClicked;
 });
