8457d10a62ff9b7b34f180088ebc2de551bb8c63
Andrew Hickman <hello@andrewhickman.me>
2021-07-25 16:27:17 -0400

    Remove iframe loader

--- public/js/script.js
+++ public/js/script.js
@@ -7,14 +7,6 @@
 const body = document.querySelector('body');
 let headerBtnClicked = false;
 
-
-/** Stop see-through loading icon in project iframe **/
-const iframe = document.querySelector('.project-embed');
-
-(() => iframe.onload = () => {
-  iframe.style.background = "none";
-})();
-
 document.querySelector('#menu-icon').addEventListener('click', e => {
   !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
   return headerBtnClicked = !headerBtnClicked;
